757b59f77cd3f1677f09595e82a1e95700a63729
Carlos Flores <cflor117@fiu.edu>
2020-09-10 16:35:44 -0400

    Added bolo models

--- bolo_flyers/models.py
+++ bolo_flyers/models.py
@@ -1,10 +1,45 @@
 from django.db import models
 
-class FieldInstances(models.Model):
-    pass
+from bolo_users.models import User
 
-class Bolos(models.Model):
-    pass
+class BoloCategory(models.Model):
+    name = models.CharField(max_length=128)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+class BoloStatus(models.Model):
+    name = models.CharField(max_length=128)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+class Bolo(models.Model):
+    
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    category = models.ForeignKey(BoloCategory, on_delete=models.PROTECT)
+    status = models.ForeignKey(BoloStatus, on_delete=models.PROTECT)
+
+    video_url = models.URLField(blank=True, null=True)
+    description = models.TextField()
+
+    reported_date = models.DateField()
+    reported_time = models.TimeField()
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    is_active = models.BooleanField(default=True)
+    is_internal = models.BooleanField(default=False)
+
+class BoloImage(models.Model):
+    bolo = models.ForeignKey(Bolo, on_delete=models.PROTECT)
+    image = models.ImageField(upload_to='bolo_images')
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
 
 class BoloSubscribers(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bolo = models.ForeignKey(Bolo, related_name="subscribed_bolos", on_delete=models.CASCADE)
+    subscribed_on = models.DateTimeField(auto_now_add=True)
